90a0926810e933734e949d4a3460771bf3532f43
Anosh Taraporevala <anoshtaraporevala@gmail.com>
2023-10-24 02:39:33 -0700

    Initial Commit

---
+++ Apple-Frameworks Project/Apple_Frameworks_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_ProjectApp.swift
+//  Apple-Frameworks Project
+//
+//  Created by Anosh Taraporevala on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
