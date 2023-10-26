f2cf2dd3a605cbf0ebb18edd4ca2255da0f34333
Anosh Taraporevala <anoshtaraporevala@gmail.com>
2023-10-25 19:44:19 -0700

    Build UI for DetailView

---
+++ Apple-Frameworks Project/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks Project
+//
+//  Created by Anosh Taraporevala on 10/25/23.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
---
+++ Apple-Frameworks Project/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks Project
+//
+//  Created by Anosh Taraporevala on 10/25/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
