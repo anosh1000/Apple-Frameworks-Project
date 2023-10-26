ea6944059f623d709b67405a0bb8a0ab99b843d2
Anosh Taraporevala <anoshtaraporevala@gmail.com>
2023-10-25 20:20:09 -0700

    Pass Data which Implements X Button using MVVM practices

--- Apple-Frameworks Project/FrameworkGridViewModel.swift
+++ Apple-Frameworks Project/FrameworkGridViewModel.swift
@@ -5,4 +5,16 @@
 //  Created by Anosh Taraporevala on 10/25/23.
 //
 
-import Foundation
+import SwiftUI
+
+final class FrameworkGridViewModel : ObservableObject{
+    
+    var selectedFramework: Framework?{
+        didSet{
+            isShowingDetailedView = true
+        }
+    }
+    
+    
+    @Published var isShowingDetailedView = false
+}
